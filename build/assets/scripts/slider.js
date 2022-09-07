2aa8195bbdb01e081fa805456b11f220cce9a9c3
litury <litury@icloud.com>
2022-09-07 16:43:10 +0500

    feat: slider index add

--- build/assets/scripts/slider.js
+++ build/assets/scripts/slider.js
@@ -1,1 +1,3 @@
-const sliderRoom1=new Image,sliderRoom2=new Image;sliderRoom1.src="assets/images/slider/slider-img-1.jpg",sliderRoom2.src="assets/images/slider/slider-img-2.jpg",document.getElementById("slider").appendChild(sliderRoom2);
+let images=[{src:"assets/images/slider/slider-img-1.jpg",title:"test1",text:"text1"},{src:"assets/images/slider/slider-img-2.jpg",title:"test2",text:"text2"}];function initSlider(){if(images&&images.length){let t=document.querySelector(".slider__images");document.querySelector(".slider__arrow");images.forEach((e,i)=>{i=`<div class="slider__image n${i} ${0===i?"slider__image_state_active":""}" 
+                           style="background-image: url(${images[i].src});">
+                       </div>`;t.innerHTML+=i}),initArrows()}}document.addEventListener("DOMContentLoaded",initSlider);
--- source/scripts/slider.js
+++ source/scripts/slider.js
@@ -1,20 +1,36 @@
-const sliderRoom1 = new Image();
-const sliderRoom2 = new Image();
-
-sliderRoom1.src = "assets/images/slider/slider-img-1.jpg";
-sliderRoom2.src = "assets/images/slider/slider-img-2.jpg";
-
 let images = [
   {
-    src: sliderRoom1,
+    src: "assets/images/slider/slider-img-1.jpg",
     title: "test1",
-    text: "text1"
+    text: "text1",
   },
   {
-    src: sliderRoom2,
+    src: "assets/images/slider/slider-img-2.jpg",
     title: "test2",
-    text: "text2"
+    text: "text2",
   },
 ];
 
-document.getElementById("slider").appendChild(sliderRoom2);
+// document.getElementById("slider").appendChild(images[1].src);
+
+function initSlider() {
+  if (!images || !images.length) return;
+
+  let sliderImages = document.querySelector(".slider__images");
+  let sliderArrows = document.querySelector(".slider__arrow");
+
+  initImages();
+  initArrows();
+
+  function initImages() {
+    images.forEach((image, index) => {
+      let imageDiv = `<div class="slider__image n${index} ${index === 0 ? "slider__image_state_active" : ""}" 
+                           style="background-image: url(${images[index].src});">
+                       </div>`;
+
+      sliderImages.innerHTML += imageDiv;
+    });
+  }
+}
+
+document.addEventListener("DOMContentLoaded", initSlider);

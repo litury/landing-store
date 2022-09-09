5507779fd078831b6d75b3701bb9d03ea8449ce3
litury <litury@icloud.com>
2022-09-09 20:32:54 +0500

    feat: slider section end

--- build/assets/scripts/slider.js
+++ build/assets/scripts/slider.js
@@ -1,3 +1,27 @@
-let images=[{src:"assets/images/slider/slider-img-1.jpg",title:"test1",text:"text1"},{src:"assets/images/slider/slider-img-2.jpg",title:"test2",text:"text2"}];function initSlider(){if(images&&images.length){let t=document.querySelector(".slider__images");document.querySelector(".slider__arrow");images.forEach((e,i)=>{i=`<div class="slider__image n${i} ${0===i?"slider__image_state_active":""}" 
-                           style="background-image: url(${images[i].src});">
-                       </div>`;t.innerHTML+=i}),initArrows()}}document.addEventListener("DOMContentLoaded",initSlider);
+let images=[{src:"assets/images/slider/slider-img-1.jpg",city:"Ekaterinburg",area:"666 m2",time:"8 years",cost:"price none"},{src:"assets/images/slider/slider-img-2.jpg",city:"Rostov-on-Don",area:"81 m2",time:"3.5 months",cost:"Upon request"}];function initSlider(t){if(images&&images.length){t=t||{title:!1,dots:!0,autoplay:!1};let s=document.querySelector(".slider__images"),e=document.querySelector(".slider__arrows"),i=document.querySelector(".slider__dots"),r=document.querySelector(".project__statistics");var a;function l(a){if(s.querySelector(".slider__image_state_active").classList.remove("slider__image_state_active"),s.querySelector(".n"+a).classList.add("slider__image_state_active"),t.dots&&(i.querySelector(".active").classList.remove("active"),i.querySelector(".n"+a).classList.add("active")),t.titles){let e=r.querySelector("#city"),t=r.querySelector("#area"),i=r.querySelector("#time"),s=r.querySelector("#cost");e.innerText=images[a].city,t.innerText=images[a].area,i.innerText=images[a].time,s.innerText=images[a].cost}}t.dots&&(images.forEach((e,t)=>{t=`<div class="slider__dots-item n${t} ${0===t?"active":""}" data-index="${t}"></div>`;i.innerHTML+=t}),i.querySelectorAll(".slider__dots-item").forEach(e=>{e.addEventListener("click",function(){l(this.dataset.index)})})),t.titles&&(a=`<div class="project__text">
+            <h3 class="title title_size_small">
+                City:
+            </h3>
+            <p class="text" id="city" id="city">${images[0].city}</p>
+        </div>
+
+        <div class="project__text">
+            <h3 class="title title_size_small">
+                apartment area:
+            </h3>
+            <p class="text text_size_small project__text-area" id="area">${images[0].area}</p>
+        </div>
+
+        <div class="project__text">
+            <h3 class="title title_size_small">
+                Repair time:
+            </h3>
+            <p class="text text_size_small" id="time">${images[0].time}</p>
+        </div>
+
+        <div class="project__text">
+            <h3 class="title title_size_small">
+                Repair Cost:
+            </h3>
+            <p class="text text_size_small" id="cost">${images[0].cost}</p>
+        </div>`,r.innerHTML+=a),t.autoplay&&setInterval(()=>{var e=+s.querySelector(".slider__image_state_active").dataset.index;l(e==images.length-1?0:1+e)},t.autoplayInterval),e.querySelectorAll(".slider__arrow").forEach(i=>{i.addEventListener("click",function(){var e=+s.querySelector(".slider__image_state_active").dataset.index;let t;l(t=i.classList.contains("slider__arrow_route_prev")?0==e?images.length-1:e-1:e==images.length-1?0:1+e)})}),images.forEach((e,t)=>{t=`<div class="slider__image n${t} ${0===t?"slider__image_state_active":""}" style="background-image: url(${images[t].src});" data-index="${t}"> </div>`;s.innerHTML+=t})}}let sliderOptions={dots:!0,titles:!0,autoplay:!0,autoplayInterval:2e3};document.addEventListener("DOMContentLoaded",function(){initSlider(sliderOptions)});
--- source/scripts/slider.js
+++ source/scripts/slider.js
@@ -1,36 +1,166 @@
 let images = [
   {
     src: "assets/images/slider/slider-img-1.jpg",
-    title: "test1",
-    text: "text1",
+    city: "Ekaterinburg",
+    area: "666 m2",
+    time: "8 years",
+    cost: "price none",
   },
   {
     src: "assets/images/slider/slider-img-2.jpg",
-    title: "test2",
-    text: "text2",
+    city: "Rostov-on-Don",
+    area: "81 m2",
+    time: "3.5 months",
+    cost: "Upon request",
   },
 ];
 
-// document.getElementById("slider").appendChild(images[1].src);
-
-function initSlider() {
+function initSlider(options) {
   if (!images || !images.length) return;
+  options = options || {
+    title: false,
+    dots: true,
+    autoplay: false,
+  };
 
   let sliderImages = document.querySelector(".slider__images");
-  let sliderArrows = document.querySelector(".slider__arrow");
+  let sliderArrows = document.querySelector(".slider__arrows");
+  let sliderDots = document.querySelector(".slider__dots");
+  let sliderInfo = document.querySelector(".project__statistics");
 
-  initImages();
+  if (options.dots) {
+    initDots();
+  }
+
+  if (options.titles) {
+    initTitle();
+  }
+
+  if (options.autoplay) {
+    initAutoplay();
+  }
+
   initArrows();
+  initImages();
 
   function initImages() {
     images.forEach((image, index) => {
-      let imageDiv = `<div class="slider__image n${index} ${index === 0 ? "slider__image_state_active" : ""}" 
-                           style="background-image: url(${images[index].src});">
-                       </div>`;
+      let imageDiv = `<div class="slider__image n${index} ${
+        index === 0 ? "slider__image_state_active" : ""
+      }" style="background-image: url(${
+        images[index].src
+      });" data-index="${index}"> </div>`;
 
       sliderImages.innerHTML += imageDiv;
     });
   }
+
+  function initArrows() {
+    sliderArrows.querySelectorAll(".slider__arrow").forEach((arrow) => {
+      arrow.addEventListener("click", function () {
+        let curNumber = +sliderImages.querySelector(
+          ".slider__image_state_active"
+        ).dataset.index;
+        let nexNumber;
+
+        if (arrow.classList.contains("slider__arrow_route_prev")) {
+          nexNumber = curNumber === 0 ? images.length - 1 : curNumber - 1;
+        } else {
+          nexNumber = curNumber === images.length - 1 ? 0 : curNumber + 1;
+        }
+        moveSlider(nexNumber);
+      });
+    });
+  }
+
+  function initDots() {
+    images.forEach((image, index) => {
+      let dot = `<div class="slider__dots-item n${index} ${
+        index === 0 ? "active" : ""
+      }" data-index="${index}"></div>`;
+      sliderDots.innerHTML += dot;
+    });
+    sliderDots.querySelectorAll(".slider__dots-item").forEach((dot) => {
+      dot.addEventListener("click", function () {
+        moveSlider(this.dataset.index);
+      });
+    });
+  }
+
+  function moveSlider(num) {
+    sliderImages
+      .querySelector(".slider__image_state_active")
+      .classList.remove("slider__image_state_active");
+    sliderImages
+      .querySelector(".n" + num)
+      .classList.add("slider__image_state_active");
+    if (options.dots) {
+      sliderDots.querySelector(".active").classList.remove("active");
+      sliderDots.querySelector(".n" + num).classList.add("active");
+    }
+    if (options.titles) changeSlider(num);
+  }
+
+  function initTitle() {
+    let sliderInfoDiv = `<div class="project__text">
+            <h3 class="title title_size_small">
+                City:
+            </h3>
+            <p class="text" id="city" id="city">${images[0].city}</p>
+        </div>
+
+        <div class="project__text">
+            <h3 class="title title_size_small">
+                apartment area:
+            </h3>
+            <p class="text text_size_small project__text-area" id="area">${images[0].area}</p>
+        </div>
+
+        <div class="project__text">
+            <h3 class="title title_size_small">
+                Repair time:
+            </h3>
+            <p class="text text_size_small" id="time">${images[0].time}</p>
+        </div>
+
+        <div class="project__text">
+            <h3 class="title title_size_small">
+                Repair Cost:
+            </h3>
+            <p class="text text_size_small" id="cost">${images[0].cost}</p>
+        </div>`;
+
+    sliderInfo.innerHTML += sliderInfoDiv;
+  }
+
+  function changeSlider(num) {
+    let sliderCity = sliderInfo.querySelector("#city");
+    let sliderArea = sliderInfo.querySelector("#area");
+    let sliderTime = sliderInfo.querySelector("#time");
+    let sliderCost = sliderInfo.querySelector("#cost");
+
+    sliderCity.innerText = images[num].city;
+    sliderArea.innerText = images[num].area;
+    sliderTime.innerText = images[num].time;
+    sliderCost.innerText = images[num].cost;
+  }
+
+  function initAutoplay() {
+    setInterval(() => {
+      let curNumber = +sliderImages.querySelector(".slider__image_state_active").dataset.index;
+      let nexNumber = curNumber === images.length - 1 ? 0 : curNumber + 1;
+      moveSlider(nexNumber)
+    }, options.autoplayInterval);
+  }
 }
 
-document.addEventListener("DOMContentLoaded", initSlider);
+let sliderOptions = {
+  dots: true,
+  titles: true,
+  autoplay: true,
+  autoplayInterval: 2000,
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  initSlider(sliderOptions);
+});
